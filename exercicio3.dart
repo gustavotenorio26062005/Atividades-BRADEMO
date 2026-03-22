180b604607d639a2e717411545567e9170ef4864
Gustavo Barros Tenório <129299486+gustavotenorio26062005@users.noreply.github.com>
2026-03-21 21:07:49 -0300

    Delete exercicio3.dart

--- exercicio3.dart
+++
@@ -1,9 +0,0 @@
-void cadastrarFuncionario({required String nome, String ?cargo}){
-  print("Funcionário $nome, Cargo: ${cargo ?? ""}");
-  
-}
-
-void main() {
-  cadastrarFuncionario(nome: "Luiz", cargo: "Gerente");
-  cadastrarFuncionario(nome: "Fulano");
-}
